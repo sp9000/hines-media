05ad41203952c2b18ea72953acab21ac5e28e89b
sp9000 <sp07@protonmail.com>
2020-07-02 07:06:04 -0400

    removed big logo and restored header-title size

--- src/@lekoarts/gatsby-theme-minimal-blog/components/header-title.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/header-title.tsx
@@ -4,7 +4,6 @@ import { jsx } from "theme-ui"
 import replaceSlashes from "../utils/replaceSlashes"
 import useSiteMetadata from "../hooks/use-site-metadata"
 import useMinimalBlogConfig from "../hooks/use-minimal-blog-config"
-import bigLogo from "../../../../static/bigLogo-blue-light.png"
 
 const HeaderTitle = () => {
   const { siteTitle } = useSiteMetadata()
@@ -16,8 +15,7 @@ const HeaderTitle = () => {
       aria-label={`${siteTitle} - Back to home`}
       sx={{ color: `heading`, textDecoration: `none` }}
     >
-      <img src={bigLogo} alt="footer logo takes you home" />
-      <h1 sx={{ my: 0, fontWeight: `medium`, fontSize: [1, 2] }}>{siteTitle}</h1>
+      <h1 sx={{ my: 0, fontWeight: `medium`, fontSize: [2, 3] }}>{siteTitle}</h1>
     </Link>
   )
 }
